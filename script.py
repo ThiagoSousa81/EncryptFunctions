3f2bf94c9fec4758166cb4a86b863828773b00b8
Thiago Sousa <thiagosousa81@gmail.com>
2023-03-02 19:46:44 -0300

    Update script.py

--- script.py
+++ script.py
@@ -5,11 +5,25 @@ class Encrypt1(object):
         return (2 * x) - 6
     def inverse(self, y: float):
         return (y /2) + 3
+    
+class Encrypt2(object):
+    def function(self, x: float):
+        return 1 / (x + 1)
+    def inverse(self, y: float):
+        return y**-1 -1
 
-F = Encrypt1()
+F1 = Encrypt1()
+F2 = Encrypt2()
 while (True):
     x = input('Digite um número: ')
     print('Número cifrado...')
-    print(F.function(float(x)))
+    print(F1.function(float(x)))
     print('Número decifrado...')
-    print(F.inverse(F.function(float(x))))
+    print(F1.inverse(F1.function(float(x))))
+    
+    x = input('Digite um número: ')
+    print('Número cifrado...')
+    print(F2.function(float(x)))
+    print('Número decifrado...')
+    print(F2.inverse(F2.function(float(x))))
+    
